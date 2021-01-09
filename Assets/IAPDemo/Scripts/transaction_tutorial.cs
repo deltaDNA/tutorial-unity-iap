dbd70601b8d57f31dba7ec9f565c8bd360bcec40
LaurieMcCulloch <lauriemcculloch@hotmail.com>
2021-01-09 16:07:17 +0000

    deltaDNA SDK Started, basic events flowing

---
+++ Assets/IAPDemo/Scripts/transaction_tutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA; 
+public class transaction_tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG); 
+        DDNA.Instance.StartSDK();
+    }
+
+}
